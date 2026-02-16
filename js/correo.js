e63412c5e954ddc9845e4fe89b011d95a0253be3
GaloGinesGarcia <galogines1@gmail.com>
2026-02-16 14:02:58 +0100

    pruebas correo V2

--- js/correo.js
+++ js/correo.js
@@ -1,4 +1,4 @@
-const RECIPIENT_EMAIL = "lory03333@gmail.com";
+const RECIPIENT_EMAIL = "aly.2497@gmail.com";
 const FORMSUBMIT_ENDPOINT = `https://formsubmit.co/ajax/${RECIPIENT_EMAIL}`;
 
 const form = document.getElementById("contactForm");
